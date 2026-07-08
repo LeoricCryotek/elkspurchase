94b37f949f79fa9b45045551dc22961f75376d9d
LeoricCryotek <dsantiago@cityoflewiston.org>
2026-07-07 22:01:01 -0700

    committ

--- __manifest__.py
+++ __manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     "name": "Elks Lodge Purchase Requests",
-    "version": "19.0.3.0",
+    "version": "19.0.3.1",
     "category": "Elks Lodge/Purchase",
     "summary": "Requisition → Board → Floor → Purchase Order approval workflow",
     "description": """
